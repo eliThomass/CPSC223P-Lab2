aee7b9119d44a188d72d0178fe30e97ea6bb2d4a
eliThomass <ecthomas05@gmail.com>
2024-09-04 14:17:48 -0700

    added credentials, added main menu printing func, added while loop for program in main.py

---
+++ contacts.py
@@ -0,0 +1,25 @@
+# Name: Eli Thomas
+# Date: 9/4/2024
+# File Purpose: 
+
+"""def print_list():
+
+def add_contact(first, last):
+	return
+
+def modify_contact(first, last):
+	return
+
+def delete_contact(first, last):
+	return
+	"""
+def print_menu():
+	print("")
+	print("     *** TUFFY TITAN CONTACT MAIN MENU ***     ")
+	print("1. Print list")
+	print("2. Add contact")
+	print("3. Modify contact")
+	print("4. Delete contact")
+	print("5. Exit program")
+	print("")
+	return
---
+++ main.py
@@ -0,0 +1,14 @@
+# Name: Eli Thomas
+# Date: 9/4/2024
+# File Purpose: main driver for contact menu
+
+from contacts import *
+
+exit_program = False
+
+while exit_program == False:
+
+	print_menu()
+	
+	choice = input("Enter menu choice: ")
+
